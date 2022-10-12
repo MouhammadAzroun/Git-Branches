7d78df780bc90ace36e6dbe3ea15e7fcea238b69
Mouhammad Azroun <mouhammadazroun@Mouhammads-MacBook-Air.local>
2022-10-12 18:44:18 +0200

    first commit on Mouhammad branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("My name is Mouhammad");
+
+        System.out.println("First code on Mouhammad branch");
     }
 }
